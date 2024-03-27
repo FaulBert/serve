92aae24a40f8c3dfdd9b5499688fbe39cae04d8b
Nazan (Nazhard) <nazhard@proton.me>
2024-03-27 22:22:06 +0700

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/nazhard/web-server
 
 go 1.21.6
+
+require github.com/fsnotify/fsnotify v1.7.0
+
+require golang.org/x/sys v0.4.0 // indirect
--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"path/filepath"
 	"strings"
+	"syscall"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-var serve = flag.String("dir", ".", "serve spesific folder.")
+var dir = flag.String("dir", ".", "serve spesific folder.")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s", r.Method, r.URL.Path)
@@ -17,7 +22,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	urlPath := r.URL.Path
 
 	if urlPath == "" || urlPath == "/" {
-		http.ServeFile(w, r, *serve+"/index.html")
+		http.ServeFile(w, r, *dir+"/index.html")
 		return
 	}
 
@@ -25,7 +30,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		urlPath += ".html"
 	}
 
-	http.ServeFile(w, r, *serve+urlPath)
+	http.ServeFile(w, r, *dir+urlPath)
 }
 
 func main() {
@@ -34,18 +39,53 @@ func main() {
 	flag.Parse()
 
 	if flag.NFlag() == 0 && (flag.Arg(0) == "-h" || flag.Arg(0) == "--help") {
-		printUsage()
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
 		os.Exit(0)
 	}
 
-	http.HandleFunc("/", handler)
-
 	fmt.Printf("Hello onii-chan! Your connection at localhost:%s nyaa~ \n", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
-}
+	go func() {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe(":"+*port, nil))
+	}()
 
-func printUsage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "Options:\n")
-	flag.PrintDefaults()
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	err = filepath.Walk(*dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			err = watcher.Add(path)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&(fsnotify.Write|fsnotify.Create) != 0 {
+					log.Println("File modified:", event.Name)
+				}
+			}
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+	log.Println("Shutting down...")
 }
